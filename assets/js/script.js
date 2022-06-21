c1b27a89b16b9cdcada6e53ab309b5880dffb4a0
Cesar Gonzalez <gcesar13@yahoo.com>
2022-06-20 19:19:58 -0600

    google like intro and save button update

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,11 +110,11 @@ function displayResults(data) {
 	for (let i = 0; i < data.results.length; i++) {
 		let result = data.results[i];
 		let html = `
-		<div>
-			<div> <img class="recipe-image" id="img_${i}" data-id="${result.id}" height="100" width="100" alt="${result.thumbnail_alt_text}" src="${result.thumbnail_url}"> </div>
-			<span>${result.name}</span>
-			<p>${result.description || ''}</p>
-			<button data-index="${i}"> Save </button>
+		<div class="box">
+		<img class="recipe-image" id="img_${i}" data-id="${result.id}" height="100" width="100" alt="${result.thumbnail_alt_text}" src="${result.thumbnail_url}">
+		<span>${result.name}</span>
+		<p>${result.description || ''}</p>
+		<button class=" button is-primary is-rounded" data-index="${i}"> Save </button>
 		</div>
 		`;
 		htmlResult.push(html);
